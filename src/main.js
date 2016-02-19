9d155e58080b65882f91a577a294b30107228fb8
Tim Kuminecz <tkuminecz@gmail.com>
2016-02-19 03:31:02 -0500

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,64 +1,147 @@
+import _ from 'lodash';
 import Reflect from 'reflect';
 
 /**
- * Converts an array of arguments into a target object
+ * See decorator spec @ https://github.com/wycats/javascript-decorators
  */
-let createTargetObject = (args) => {
-	let targetObj = {
-		target: args[0]
-	};
 
-	if (args.length > 1) {
-		targetObj.prop = args[1];
-		targetObj.descriptor = args[2];
+const has = _.has;
+
+// Returns true if the target is a class
+let isClass = (args) => {	
+	if (args.length === 1 && args[0] instanceof Function) {
+		return true;
 	}
 
-	return targetObj;
+	return false;
 };
 
-/**
- * Helper method to dispatch decoration of
- * a class to the decorator class.
- *
- * @return {Object} [The decorated class constructor]
- */
-let decorateClass = (decoratorClass, targetClass, params=[]) => {
-	return decoratorClass.decorateClass.apply(decoratorClass, [targetClass].concat(params));
-};
-
-/**
- * Helper method to dispatch decoration of a
- * class method/property to the decorator class.
- * 
- * @return {Object} [The decorated property descriptor]
- */
-let decorateProperty = () => {
-
-};
-
-/**
- * Decorates a class to behave as a decorator. The class
- * being decorated must have a `decorate` method.
- *
- * @return {Object} [The decorator]
- */
-let decorator = (target) => {
-	let instance = Reflect.construct(target);
-
-	return (...args) => {
-		console.log('decorator args', args, typeof args[0]);
-		if (args[0] instanceof Function) { // plain @decorator style
-			console.log('plain decorator');
-			return decorateClass(instance, createTargetObject(args));
+// Returns true if the target is a class property
+let isProperty = (args) => {
+	if (args.length === 3) {
+		let desc = args[2];
+		if (has(desc, 'enumerable') && has(desc, 'configurable') && has(desc, 'writable')) {
+			if (desc.enumerable === true) {
+				return true;
+			}
 		}
-		else { // invoked @decorator(param1, param2) style
-			console.log('invoked style');
-			return (...targetArgs) => {
-				console.log('target args', targetArgs);
-				return decorateClass(instance, createTargetObject(targetArgs), args);
+	}
+
+	return false;
+};
+
+// Returns true if the target is a class method
+let isMethod = (args) => {	
+	if (args.length === 3) {
+		let desc = args[2];
+		if (has(desc, 'writable') && has(desc, 'enumerable') && has(desc, 'configurable')) {
+			if (desc.enumerable === false) {
+				return true;
+			}
+		}
+	}
+};
+
+// Returns true if the target is an accessor method
+let isAccessor = (args) => {
+	if (args.length === 3) {
+		let desc = args[2];
+		if (has(desc, 'get') && has(desc, 'set')) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+// Calls a method or throws if it doens't exist
+let callMethod = (obj, method, args) => {
+	if (typeof obj[method] !== 'function') {
+		throw new Error(`Decorator ${obj} can't be used to decorate classes.`);
+	}
+
+	return Reflect.apply(obj[method], obj, args);
+};
+
+// Returns true if the the decorator isn't parametric
+let isPlainStyle = (target) => {
+	return (isClass(target) || isMethod(target) || isAccessor(target) || isProperty(target));
+};
+
+// Dispatches to the appropriate decorate method
+let dispatchDecorate = (instance, target, params=[]) => {
+	if (isClass(target)) {
+		return callMethod(instance, 'decorateClass', target.concat(params));
+	}
+	else if (isMethod(target)) {
+		return callMethod(instance, 'decorateMethod', target.concat(params));
+	}
+	else if (isAccessor(target)) {
+		return callMethod(instance, 'decorateAccessor', target.concat(params));
+	}
+	else if (isProperty(target)) {
+		return callMethod(instance, 'decorateProperty', target.concat(params));
+	}
+	else {
+		throw new Error(`Unable to detect decoration type.`);
+	}
+};
+
+/**
+ * Decorates a class to behave as a decorator.
+ */
+let decorator = (decoratorClass) => {
+	let instance = Reflect.construct(decoratorClass);
+
+	return (...target) => {
+		if (isPlainStyle(target)) {
+			dispatchDecorate(instance, target);
+		}
+		else { // parametric style
+			let params = target;
+			return (...target) => {
+				return dispatchDecorate(instance, target, params);
 			};
 		}
 	};
 };
+
+@decorator
+class Foo {
+
+	decorateClass(targetClass, ...params) {
+		console.log('decorateClass()', targetClass, params);
+	}
+
+	decorateProperty(targetPrototype, prop, descriptor, ...params) {
+		console.log('decorateProperty()', targetPrototype, prop, descriptor, params);
+	}
+
+	decorateAccessor(targetPrototype, prop, descriptor, ...params) {
+		console.log('decorateAccessor()', targetPrototype, prop, descriptor, params);
+	}
+
+	decorateMethod(proto, prop, desc, ...params) {
+		console.log('decorateMethod()', proto, prop, desc, params);
+	}
+}
+
+
+@Foo
+class Bar {
+
+	@Foo
+	property = {};
+
+	@Foo
+	method() {
+	}
+
+	@Foo
+	get accessor() {
+	} 
+
+}
+
 
 export default decorator;
